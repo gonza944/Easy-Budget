d0f336f1031af82530c03a46355dee760e592bd1
Gonzalo Ariza <gonza94.4@gmail.com>
2025-06-19 17:14:27 -0300

    test supabase change

--- server/supabaseConnection.ts
+++ server/supabaseConnection.ts
@@ -7,7 +7,14 @@ if (!process.env.SUPABASE_KEY) {
   throw new Error("Environment variable SUPABASE_KEY is not defined.");
 }
 
+// Default client with anon key (for public operations)
 export const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
+  process.env.SUPABASE_KEY,
+  {
+    auth: {
+      autoRefreshToken: true,
+      persistSession: true // Important for server-side usage
+    },
+  }
+);;
