973d9b9c768a27ebbda61bc90dccf86d311e6210
Gonzalo Ariza <gonza94.4@gmail.com>
2025-07-03 14:53:16 -0300

    Refactor filterDrawer to improve date selection handling
    - Updated DateSelector to close the drawer on date selection.
    - Moved "Today" button functionality to a dedicated method for better readability and modularity.
    - Ensured consistent formatting in the template for improved code clarity.

--- components/filterDrawer.vue
+++ components/filterDrawer.vue
@@ -1,12 +1,12 @@
 <template>
-   <Drawer :open="isOpen" @update:open="isOpen = $event">
+  <Drawer :open="isOpen" @update:open="isOpen = $event">
     <DrawerContent class="flex flex-col gap-4 items-center justify-center pb-8">
       <DrawerHeader>
         <DrawerTitle class="text-2xl font-bold">Select Date</DrawerTitle>
       </DrawerHeader>
 
-      <DateSelector v-model:selectedDate="selectedDate" />
-      <Button variant="default" class="w-xs" @click="selectedDate = new Date()">Today</Button>
+      <DateSelector v-model:selectedDate="selectedDate" @click="isOpen = false" />
+      <Button variant="default" class="w-xs" @click="onTodayClick">Today</Button>
 
     </DrawerContent>
   </Drawer>
@@ -16,6 +16,11 @@
 
 const isOpen = defineModel<boolean>('isOpen')
 const selectedDate = defineModel<Date>('selectedDate')
+
+const onTodayClick = () => {
+  selectedDate.value = new Date()
+  isOpen.value = false
+}
 
 </script>
 
