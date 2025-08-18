525824d78661083cf71f8ed824d0da8ea96eb276
Gonzalo Ariza <gonza94.4@gmail.com>
2025-08-18 09:47:22 -0300

    update: background height

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full min-h-screen bg-main-gradient text-foreground md:px-4">
+  <div class="w-full min-h-[100dvh] bg-main-gradient text-foreground md:px-4">
     <main>
       <slot />
     </main>
